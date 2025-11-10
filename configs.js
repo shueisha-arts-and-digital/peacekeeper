8b5d4e6d0ce0905b69ba9751a03f485646ce5083
Yuki Adachi <yuki777@gmail.com>
2025-11-10 10:54:12 +0700

    Support S3 bucket match

--- configs.js
+++ configs.js
@@ -71,7 +71,7 @@ export function getDefaultConfigs() {
       },
       url: "^https://github.com/shueisha-arts-and-digital/(backlog-mcp-server|composer-aws-assume-role-plugin|dbdb|docbase-mcp-server|github-backlog-sync|image|ms-teams-notify-orb|peacekeeper|Pj8.SentryModule|qiq-vscode-extension|sssh|vscode-bear-goto|vscode-twig-goto)",
     },
-    // S3
+    // S3 (params)
     {
       name: "development",
       alert: false,
@@ -103,6 +103,36 @@ export function getDefaultConfigs() {
       style: "#app > div > div > div > section { background-color: rgba(253, 129, 122, 1) !important; }",
       url: "^https://.*\\.console\\.aws\\.amazon\\.com/s3/.*\\?.*prefix=prod(%2F|/)",
       includeQueryParams: true,
+    },
+    // S3 (bucket name)
+    {
+      name: "development",
+      alert: false,
+      favicon: false,
+      style: "#app > div > div > div > section { background-color: rgba(99, 187, 253, 1) !important; }",
+      url: "^https://.*\\.console\\.aws\\.amazon\\.com/s3/.*(dev).*",
+    },
+    {
+      name: "staging",
+      alert: false,
+      favicon: {
+        rel: "icon",
+        type: "image/svg+xml",
+        href: "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' width='16' height='16'%3E%3Cpath d='M8 1 L15 14 L1 14 Z' fill='%23FFC107' stroke='%23F57F17' stroke-width='0.5'/%3E%3C/svg%3E",
+      },
+      style: "#app > div > div > div > section { background-color: rgba(255,214,10,1) !important; }",
+      url: "^https://.*\\.console\\.aws\\.amazon\\.com/s3/.*(stg|stage|staging).*",
+    },
+    {
+      name: "production",
+      alert: false,
+      favicon: {
+        rel: "icon",
+        type: "image/svg+xml",
+        href: "data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 16 16' width='16' height='16'%3E%3Cpath d='M8 1 L15 14 L1 14 Z' fill='%23F44336' stroke='%23B71C1C' stroke-width='0.5'/%3E%3C/svg%3E",
+      },
+      style: "#app > div > div > div > section { background-color: rgba(253, 129, 122, 1) !important; }",
+      url: "^https://.*\\.console\\.aws\\.amazon\\.com/s3/.*(prd|prod).*",
     },
     // ECS
     {
